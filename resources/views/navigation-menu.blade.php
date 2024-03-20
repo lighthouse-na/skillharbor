89d8e2dde735cacffedb4c860f3fb613adad662c
hpmouton <hmouton786@gmail.com>
2024-03-20 12:38:51 +0200

    UI Update ready for contributtions

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-white sticky top-0 dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-24">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
