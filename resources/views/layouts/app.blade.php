0c1514b47cad8f76a4f5a09256cfcff738e164f0
hpmouton <hmouton786@gmail.com>
2024-03-20 11:25:24 +0200

    UI Update: Blade icons dependency added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,41 +1,80 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!--Favicon-->
+    <link rel="shortcut icon" href="{{ asset('images/logos/telecom_namibia_logo.png') }}">
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-rubik antialiased">
-        <x-banner />
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
 
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+    @livewireScripts
+    @stack('scripts')
+</head>
+
+<body class="font-sans antialiased bg-white">
+    <div class="relative min-h-screen md:flex " x-data="{ open: false }">
+        <!--Sidebar -->
+        <aside :class="{ '-translate-x-full': !open }"
+            class="z-10 bg-gradient-to-r from-cyan-800 to-slate-800 lg:fixed sm:fixed text-gray-900 w-56 px-2 py-4  absolute inset-y-0 left-0 top-16 bottom-0 transform md:translate-x-0 overflow-y-auto transition ease-in-out duration-200 shadow-lg">
+            <!--logo-->
+            <div class="flex items-center justify-between px-2">
+
+
+                <button type="button" @click="open = !open"
+                    class="md:hidden inline-flex p-2 items-center justify-center rounded-md text-black hover:bg-orange-400 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="block w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+
+
+            <!--Nav Links-->
+            <nav>
+                <x-side-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" icon="home">
+                    Dashboard
+                </x-side-nav-link>
+
+
+            </nav>
+        </aside>
+
+
+        <!-- Page Content -->
+        <main class="flex-1 bg-gradient-to-r from-gray-100 to-stone-200 min-h-screen">
             @livewire('navigation-menu')
+            <div class="mx-auto ml-44">
+                <div class="w-5/6">
+                    {{ $slot }}
+                </div>
+                {{-- {{ $slot }} --}}
+            </div>
 
+        </main>
+    </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+</body>
+<footer class=" py-4 text-center text-slate-800">
+    <p class="text-sm">
+        SkillHarbor developed by Lighthouse &copy; {{ date('Y') }}
+    </p>
+</footer>
 
-        @stack('modals')
-
-        @livewireScripts
-    </body>
 </html>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<div class="bg-white sticky top-0 dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -10,11 +10,7 @@
                     </a>
                 </div>
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="text-gray-800 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-100">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
+
             </div>
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Settings Dropdown -->
