42026ee891248661cf1794ef5cb6b1463c5cc8d5
aaron-muti-420 <mutiaaron2@gmail.com>
2024-03-25 20:25:49 +0200

    pulled changes

---
+++ storage/framework/views/578f61f141cbfb5cbfb2bebe42290f19.php
@@ -0,0 +1,57 @@
+<?php $attributes ??= new \Illuminate\View\ComponentAttributeBag; ?>
+<?php foreach($attributes->onlyProps(['submit']) as $__key => $__value) {
+    $$__key = $$__key ?? $__value;
+} ?>
+<?php $attributes = $attributes->exceptProps(['submit']); ?>
+<?php foreach (array_filter((['submit']), 'is_string', ARRAY_FILTER_USE_KEY) as $__key => $__value) {
+    $$__key = $$__key ?? $__value;
+} ?>
+<?php $__defined_vars = get_defined_vars(); ?>
+<?php foreach ($attributes as $__key => $__value) {
+    if (array_key_exists($__key, $__defined_vars)) unset($$__key);
+} ?>
+<?php unset($__defined_vars); ?>
+
+<div <?php echo e($attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6'])); ?>>
+    <?php if (isset($component)) { $__componentOriginal6a0a1523cc2edf33c83fe20a5d1f7f78 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal6a0a1523cc2edf33c83fe20a5d1f7f78 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.section-title','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('section-title'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(Illuminate\View\AnonymousComponent::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+         <?php $__env->slot('title', null, []); ?> <?php echo e($title); ?> <?php $__env->endSlot(); ?>
+         <?php $__env->slot('description', null, []); ?> <?php echo e($description); ?> <?php $__env->endSlot(); ?>
+     <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal6a0a1523cc2edf33c83fe20a5d1f7f78)): ?>
+<?php $attributes = $__attributesOriginal6a0a1523cc2edf33c83fe20a5d1f7f78; ?>
+<?php unset($__attributesOriginal6a0a1523cc2edf33c83fe20a5d1f7f78); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal6a0a1523cc2edf33c83fe20a5d1f7f78)): ?>
+<?php $component = $__componentOriginal6a0a1523cc2edf33c83fe20a5d1f7f78; ?>
+<?php unset($__componentOriginal6a0a1523cc2edf33c83fe20a5d1f7f78); ?>
+<?php endif; ?>
+
+    <div class="mt-5 md:mt-0 md:col-span-2">
+        <form wire:submit="<?php echo e($submit); ?>">
+            <div class="px-4 py-5 bg-white dark:bg-gray-800 sm:p-6 shadow <?php echo e(isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'); ?>">
+                <div class="grid grid-cols-6 gap-6">
+                    <?php echo e($form); ?>
+
+                </div>
+            </div>
+
+            <!--[if BLOCK]><![endif]--><?php if(isset($actions)): ?>
+                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 dark:bg-gray-800 text-end sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                    <?php echo e($actions); ?>
+
+                </div>
+            <?php endif; ?><!--[if ENDBLOCK]><![endif]-->
+        </form>
+    </div>
+</div>
+<?php /**PATH /home/wings/Telecom/skillharbor-open/resources/views/components/form-section.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/82bbc06479a70ec0add63b05d5d2509c.php
@@ -0,0 +1,30 @@
+<div class="bg-white sticky top-0 dark:bg-gray-800  dark:border-gray-700 ">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-20">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="shrink-0 flex items-center">
+                    <a href="<?php echo e(route('dashboard')); ?>" class="text-black dark:text-gray-200">
+                        <h1 class="block text-3xl w-auto">skillHarbor</h1>
+                    </a>
+                </div>
+                <!-- Navigation Links -->
+
+            </div>
+            
+            <!-- Hamburger -->
+            <div class="-me-2 flex items-center sm:hidden">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+
+            
+        </div>
+    </div>
+</div>
+<?php /**PATH /home/wings/Telecom/skillharbor-open/resources/views/navigation-menu.blade.php ENDPATH**/ ?>
