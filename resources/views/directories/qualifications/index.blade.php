7f8e87e36affeb39f1ef998f08f3ce982bbb9a41
aaron-muti-420 <mutiaaron2@gmail.com>
2024-03-26 12:25:09 +0200

    routs

---
+++ resources/views/directories/qualifications/index.blade.php
@@ -0,0 +1,12 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="">
+            {{ __('Assesment Directory') }}
+        </h2>
+    </x-slot>
+    <div class="py-6 px-6">
+        <div class="">
+        </div>
+    </div>
+</x-app-layout>
