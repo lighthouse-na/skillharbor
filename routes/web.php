729bae66ede6e6d44304629bfed8136a276f2a88
hpmouton <hmouton786@gmail.com>
2024-03-26 10:30:57 +0200

    Updated naming conventions for completing assessments from assessment to user-assessment

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 
-    //Assessment Routes
-    Route::get('/assessment/{user}', [AssessmentController::class, 'index'])->name('assessment');
-    Route::get('/assessment/{user}/{assessment}', [AssessmentController::class, 'show'])->name('assessment.show');
-    Route::post('/assessment/{user}/{assessment}/{jcp}', [AssessmentController::class, 'storeEmployee'])->name('assessment.storeEmployee');
+    //Completing an Assessment Routes
+    Route::get('/user-assessment/{user}', [AssessmentController::class, 'index'])->name('user-assessment');
+    Route::get('/user-assessment/{user}/{assessment}', [AssessmentController::class, 'show'])->name('user-assessment.show');
+    Route::post('/user-assessment/{user}/{assessment}/{jcp}', [AssessmentController::class, 'storeEmployee'])->name('user-assessment.storeEmployee');
 
     //Skill Internal API Routes
     Route::resource('/skills', SkillController::class);
