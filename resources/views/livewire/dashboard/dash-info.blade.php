275a25d250b72af021b7093efd99da38eb5f3c81
hpmouton <hmouton786@gmail.com>
2024-04-02 12:10:28 +0200

    Dashboard UI Update

--- resources/views/livewire/dashboard/dash-info.blade.php
+++ resources/views/livewire/dashboard/dash-info.blade.php
@@ -1,12 +1,22 @@
 <div class="p-4">
     <div class="p-4 rounded-lg dark:border-gray-700">
        <div class="grid grid-cols-3 gap-4 mb-4">
-          <div class="flex items-center justify-center border h-32 rounded bg-gray-50 dark:bg-gray-800">
-             <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-                </svg>
-             </p>
+          <div class="flex flex-col h-auto">
+            <div class="flex mb-1 grow items-center justify-center border rounded-lg bg-gray-50 dark:bg-gray-800">
+                <p class="text-2xl text-gray-400 dark:text-gray-500">
+                   <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                   </svg>
+                </p>
+             </div>
+             <div class="flex grow items-center justify-center border rounded-lg bg-gray-50 dark:bg-gray-800">
+                <p class="text-2xl text-gray-400 dark:text-gray-500">
+                   <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                   </svg>
+                </p>
+             </div>
+
           </div>
           <div class="border h-auto rounded-lg dark:bg-gray-800">
             <div class="flex flex-row justify-between items-center px-6 pt-3">
