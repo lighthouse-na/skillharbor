7542800d8e39ab476c49029eb2732972bac62bf1
hpmouton <hmouton786@gmail.com>
2024-04-05 16:29:10 +0200

    UI Update

--- resources/views/livewire/system/jcps/j-c-p-create-form.blade.php
+++ resources/views/livewire/system/jcps/j-c-p-create-form.blade.php
@@ -20,8 +20,15 @@
 
         </div>
         <div class="col-span-6 sm:col-span-4">
+            <x-label for="duty_station" value="{{ __('Duty Station') }}" />
+            <x-input id="duty_station" type="text" class="mt-1 block w-full" wire:model="state.duty_station" required autocomplete="duty_station" />
+            <x-input-error for="duty_station" class="mt-2" />
+
+
+        </div>
+        <div class="col-span-6 sm:col-span-4">
             <x-label for="job_grade" value="{{ __('Job Grade') }}" />
-            <select id="job_grade" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select id="job_grade" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-fuchsia-500 focus:border-fuchsia-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-500 dark:focus:border-fuchsia-500">
               <option selected>Choose the job grade.</option>
               <option value="a1">A1</option>
               <option value="a2">A2</option>
@@ -43,18 +50,19 @@
         <!-- Job Purpose -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="job_prupose" value="{{ __('Job Purpose') }}" />
-            <textarea id="job_purpose" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+            <textarea id="job_purpose" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-fuchsia-500 focus:border-fuchsia-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-500 dark:focus:border-fuchsia-500" placeholder="Write your thoughts here..."></textarea>
         </div>
 
         <div class="col-span-6 sm:col-span-4">
             <label class="inline-flex items-center cursor-pointer">
                 <input type="checkbox" value="" class="sr-only peer">
-                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-fuchsia-300 dark:peer-focus:ring-fuchsia-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-fuchsia-600"></div>
                 <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">JCP Active</span>
               </label>
 
 
         </div>
+
     </x-slot>
 
     <x-slot name="actions">
