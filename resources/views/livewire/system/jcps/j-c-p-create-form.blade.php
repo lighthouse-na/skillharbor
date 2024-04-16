a809a58e5339b8847049d2028e26f6c30b499f46
hpmouton <hmouton786@gmail.com>
2024-04-16 16:05:17 +0200

    Updated UI

--- resources/views/livewire/system/jcps/j-c-p-create-form.blade.php
+++ resources/views/livewire/system/jcps/j-c-p-create-form.blade.php
@@ -16,21 +16,21 @@
         <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="position_title" value="{{ __('Position Title') }}" />
-            <x-input id="position_title" type="text"  class="mt-1 block w-full" wire:model="state.position_title" required autocomplete="position_title" />
+            <x-input id="position_title" type="text"  class="mt-1 block w-full" wire:model="position_title" required autocomplete="position_title" />
             <x-input-error for="position_title" class="mt-2" />
 
 
         </div>
         <div class="col-span-6 sm:col-span-4">
             <x-label for="duty_station" value="{{ __('Duty Station') }}" />
-            <x-input id="duty_station" type="text" class="mt-1 block w-full" wire:model="state.duty_station" required autocomplete="duty_station" />
+            <x-input id="duty_station"  type="text" class="mt-1 block w-full" wire:model="duty_station" required autocomplete="duty_station" />
             <x-input-error for="duty_station" class="mt-2" />
 
 
         </div>
         <div class="col-span-6 sm:col-span-4">
             <x-label for="job_grade" value="{{ __('Job Grade') }}" />
-            <select id="job_grade" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-fuchsia-500 focus:border-fuchsia-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-500 dark:focus:border-fuchsia-500">
+            <select id="job_grade" wire:model="job_grade" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-fuchsia-500 focus:border-fuchsia-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-500 dark:focus:border-fuchsia-500">
               <option selected>Choose the job grade.</option>
               <option value="a1">A1</option>
               <option value="a2">A2</option>
@@ -57,12 +57,12 @@
         <!-- Job Purpose -->
         <div class="col-span-6 sm:col-span-4">
             <x-label for="job_prupose" value="{{ __('Job Purpose') }}" />
-            <textarea id="job_purpose" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-fuchsia-500 focus:border-fuchsia-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-500 dark:focus:border-fuchsia-500" placeholder="Write your thoughts here..."></textarea>
+            <textarea id="job_purpose" wire:model="job_purpose"  rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-fuchsia-500 focus:border-fuchsia-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-500 dark:focus:border-fuchsia-500" placeholder="Write your thoughts here..."></textarea>
         </div>
 
         <div class="col-span-6 sm:col-span-4">
             <label class="inline-flex items-center cursor-pointer">
-                <input type="checkbox" value="" class="sr-only peer">
+                <input type="checkbox" wire:model="is_active" value="" class="sr-only peer">
                 <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-fuchsia-300 dark:peer-focus:ring-fuchsia-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-fuchsia-600"></div>
                 <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">JCP Active</span>
               </label>
@@ -74,7 +74,7 @@
         <div class="col-span-full sm:col-span-4">
                 <x-label for="qualifications" value="{{ __('Qualifications') }}" />
 
-                <select id="jcp_qualifications.qualification_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select id="jcp_qualifications.qualification_id" wire:model="jcp_qualifications.qualification_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                   <option selected>Choose Qualifications</option>
                 @forelse ($qualifications as $qualification)
                     <option value="{{ $qualification->id }}">{{ $qualification->qualification_title }}</option>
@@ -118,8 +118,8 @@
             <tbody>
             @foreach ($jcp_skills as $index => $jcp_skills)
             <tr>
-                <td>
-                    <select name="skills" wire:model="jcp_skills.{{$index}}.skill_id" class="w-full form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <select name="skills" wire:model="jcp_skills.{{$index}}.skill_id" class="rounded-lg">
                         @foreach ($skills as $skill)
                             <option value={{$skill->id}}>
                                 {{$skill->skill_title}}
@@ -127,9 +127,8 @@
                         @endforeach
                     </select>
                 </td>
-                <td>
-                    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-                    <select id="countries" name="jcp_skills.{{$index}}.required_rating" wire:model="jcp_skills.{{$index}}.required_rating" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <select id="countries" name="jcp_skills.{{$index}}.required_rating" wire:model="jcp_skills.{{$index}}.required_rating" class="rounded-lg">
                       <option selected>Choose a required rating</option>
                       <option value="1">United States</option>
                       <option value="2">Canada</option>
@@ -138,8 +137,8 @@
                       <option value="5">Germany</option>
                     </select>
                 </td>
-                <td>
-                    <a href='#' wire:click.prevent="removeSkill({{$index}})">Delete</a>
+                <td class="px-3">
+                    <a href='#' class="text-red-500" wire:click.prevent="removeSkill({{$index}})">Delete</a>
                 </td>
             </tr>
 
@@ -148,9 +147,9 @@
 
             </tbody>
         </table>
-        <div class="">
+        <div class="mt-6">
             <div class="">
-                <x-button class="" wire:click.prevent="addSkill">+ Add Skill</x-button>
+                <x-button class="" wire:click.prevent="addSkill">Add Skill</x-button>
             </div>
         </div>
         </div>
