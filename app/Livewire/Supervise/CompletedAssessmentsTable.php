8a2e2f9e891e7a8f59ba81f3a7cf1aa0f037f493
hpmouton <hmouton786@gmail.com>
2024-04-24 16:26:36 +0200

    Supervisor Component added

---
+++ app/Livewire/Supervise/CompletedAssessmentsTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Supervise;
+
+use Livewire\Component;
+
+class CompletedAssessmentsTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.supervise.completed-assessments-table');
+    }
+}
