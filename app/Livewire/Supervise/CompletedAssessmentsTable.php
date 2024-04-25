d6e50c5b7e2be9eb00bcc2fb4df4e1ddc5c83ff9
hpmouton <hmouton786@gmail.com>
2024-04-25 09:16:06 +0200

    Supervise backend shows users with updated user status

--- app/Livewire/Supervise/CompletedAssessmentsTable.php
+++ app/Livewire/Supervise/CompletedAssessmentsTable.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire\Supervise;
 
+use App\Models\User;
 use Livewire\Component;
 
 class CompletedAssessmentsTable extends Component
 {
+
+
     public function render()
     {
-        return view('livewire.supervise.completed-assessments-table');
+        $users = User::whereHas('enrolled', function ($query) {
+            $query->where('user_status', 1);
+        })->get();
+
+        return view('livewire.supervise.completed-assessments-table', compact('users'));
     }
 }
