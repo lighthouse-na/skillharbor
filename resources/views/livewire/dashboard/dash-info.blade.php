13f09b4398c7aa066722345efc114e1ad037f7cc
hpmouton <hmouton786@gmail.com>
2024-04-26 17:15:43 +0200

    UI Update

--- resources/views/livewire/dashboard/dash-info.blade.php
+++ resources/views/livewire/dashboard/dash-info.blade.php
@@ -166,12 +166,12 @@
                 </div>
              </div>
           </div>
-          <div class="flex items-center justify-center rounded bg-gray-50 border h-auto dark:bg-gray-800">
+          <div class="flex items-center justify-center rounded border h-auto dark:bg-gray-800">
             <p class="text-2xl text-gray-400 dark:text-gray-500">
-               <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-               </svg>
-            </p>
+                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                </svg>
+             </p>
          </div>
           <div class="flex items-center justify-center rounded bg-gray-50 border h-auto dark:bg-gray-800">
             <p class="text-2xl text-gray-400 dark:text-gray-500">
@@ -272,7 +272,9 @@
                 }
               }
             });
+
           </script>
+
           @endscript
 
 </div>
