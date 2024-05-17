ca46239a9eb9bf00279b290b57d24806763c4faf
hpmouton <hmouton786@gmail.com>
2024-05-17 12:27:24 +0200

    Reports Page UI Updates

---
+++ app/Livewire/Reports/SystemReports.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Reports;
+
+use Livewire\Component;
+
+class SystemReports extends Component
+{
+    public function render()
+    {
+        return view('livewire.reports.system-reports');
+    }
+}
