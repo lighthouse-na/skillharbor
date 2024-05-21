3ea5a6be580b0944c7b4624b82c42257ca861ad9
Hubert Mouton <219079714@nust.na>
2024-05-21 10:56:27 +0200

    Update edit.blade.php

--- resources/views/directories/assessments/edit.blade.php
+++ resources/views/directories/assessments/edit.blade.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Crypt;
 @endphp
 <x-app-layout>
 <div class="mt-6 max-w-lg mx-auto">
-    <form action="{{ route('assessments.update', Crypt::decryptString($encryptedId)) }}" method="POST">
+    <form action="{{ route('assessments.update', Crypt::encrypt($assessment->id)) }}" method="POST">
         
         @method('PUT')
         @csrf
