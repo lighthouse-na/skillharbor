12ea1587f98f5dbb251f83467a6460bb4311dbeb
Aaron Markus <mutiaaron2@gmail.com>
2024-05-27 16:22:13 +0200

    Updated Version of JCP update

--- resources/views/directories/jcp/edit.blade.php
+++ resources/views/directories/jcp/edit.blade.php
@@ -56,7 +56,7 @@
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <label class="inline-flex items-center cursor-pointer">
-                    <input type="checkbox" wire:model="is_active" value="" class="sr-only peer">
+                    <input type="checkbox" name="is_active" id="is_active"class="sr-only peer">
                     <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-fuchsia-300 dark:peer-focus:ring-fuchsia-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-fuchsia-600"></div>
                     <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">JCP Active</span>
                   </label>
