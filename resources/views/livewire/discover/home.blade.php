9f94ae480dfed0208fd9e6fb47a2418c1deb6ef2
hpmouton <hmouton786@gmail.com>
2024-05-27 09:34:01 +0200

    Dev setup

--- resources/views/livewire/discover/home.blade.php
+++ resources/views/livewire/discover/home.blade.php
@@ -1,9 +1,9 @@
 <div class="h-screen">
-    <div class="grid grid-cols-5 mx-12 gap-3 mt-3">
+    <div class="grid grid-cols-3 mx-12 gap-6 mt-3">
 
     @forelse ($assessments as $assessment)
 
-    <a href="#" class="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 space-y-3 hover:bg-gradient-to-b from-fuchsia-900 to-fuchsia-600  transition hover:duration-700 ease-in-out">
+    <a href="#" class="group block w-full mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 space-y-3 hover:bg-gradient-to-b from-fuchsia-900 to-fuchsia-600  transition hover:duration-700 ease-in-out">
 
         <div class="flex items-center space-x-3">
          <x-iconoir-doc-star class="h-6 w-6 text-fuchsia-500 group-hover:text-white" />
