7a33826e47f1f4e0100b561f79367aa96a341898
hpmouton <hmouton786@gmail.com>
2024-09-18 14:55:08 +0200

    UI Update in progress

--- resources/views/livewire/dashboard/dash-info.blade.php
+++ resources/views/livewire/dashboard/dash-info.blade.php
@@ -13,39 +13,9 @@
                             class="flex flex-col items-center justify-around mx-auto w-32 h-32 relative -mt-16 border-4 border-white bg-fuchsia-950 rounded-full overflow-hidden">
                             <h1 class="text-white text-3xl font-bold">{{ $user->competency_rating }}</h1>
                         </div>
-                        <div class="text-center mt-2">
+                        <div class="text-center my-3">
                             <h2 class="font-semibold">{{ $user->first_name }} {{ $user->last_name }}</h2>
                             <p class="text-gray-500">{{ $user->email }}</p>
-                        </div>
-                        <ul class="py-4 mt-2 text-gray-700 flex items-center justify-around">
-                            <li class="flex flex-col items-center justify-around">
-                                <svg class="w-4 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20">
-                                    <path
-                                        d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                                </svg>
-                                <div>{{ $user->qualifications->count() }}</div>
-                            </li>
-                            <li class="flex flex-col items-center justify-between">
-                                <svg class="w-4 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20">
-                                    <path
-                                        d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z" />
-                                </svg>
-                                <div>{{ $user->assessments->count() }}</div>
-                            </li>
-                            <li class="flex flex-col items-center justify-around">
-                                <svg class="w-4 fill-current text-blue-900" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20">
-                                    <path
-                                        d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
-                                </svg>
-                                <div>{{ $user->jcp->count() }}</div>
-                            </li>
-                        </ul>
-                        <div class="p-4 border-t mx-8 mt-2">
-                            <button
-                                class="w-1/2 block mx-auto rounded-full bg-fuchsia-900 hover:bg-fuchsia-950 font-semibold text-white px-6 py-2  transition ease-in-out duration-150">Profile</button>
                         </div>
                     </div>
                 </div>
