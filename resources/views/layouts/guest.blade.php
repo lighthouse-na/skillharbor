797d7634bb7f1d84a27479e5889a5663ab262c56
hpmouton <hmouton786@gmail.com>
2024-10-15 16:12:23 +0200

    Landing Page Update: - Database Migration files cleanup

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,22 +8,28 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
+        <!--Favicon-->
         <link rel="shortcut icon" href="{{ asset('assets/logo/logo.png') }}">
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <!-- Styles -->
         @livewireStyles
+        @livewireScripts
+        @stack('scripts')
     </head>
     <body>
-        <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
+        <div class="font-rubik text-gray-900 dark:text-gray-100 antialiased">
             {{ $slot }}
         </div>
 
-        @livewireScripts
     </body>
 </html>
