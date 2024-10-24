19ceddaa8b3d34dc2ff6fdcae29f7d06cc593d56
hpmouton <hmouton786@gmail.com>
2024-10-24 16:34:32 +0200

    Fresh As Ducks

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -6,38 +6,38 @@
     </x-slot>
 
     <div class="ml-4">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-md rounded-3xl border">
             <div>
                 <header class="">
-                    <div class="font-medium py-6 bg-white text-3xl text-gray-500 leading-tight sm:px-6 lg:px-8">
+                    <div class="font-medium py-6 bg-sky-950 text-3xl text-white leading-tight sm:px-6 lg:px-8">
                         <nav class="flex" aria-label="Breadcrumb">
                             <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
                                 <li class="inline-flex items-center">
-                                    <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-fuchsia-600 dark:text-gray-400 dark:hover:text-fuchsia-600">
+                                    <a href="#" class="inline-flex items-center text-sm font-medium text-white hover:text-sky-200 dark:text-gray-400 dark:hover:text-sky-600">
                                         <h1 class="mr-1">@svg('iconoir-reports')</h1>
                                         Reports
                                     </a>
                                 </li>
                                 <li>
                                     <div class="flex items-center">
-                                        <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                                        <svg class="rtl:rotate-180 w-3 h-3 text-white mx-1" aria-hidden="true"
                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                             <path stroke="currentColor" stroke-linecap="round"
                                                 stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                                         </svg>
-                                        <a href="#" class="ms-1 text-sm font-medium text-gray-700 hover:text-fuchsia-600 md:ms-2 dark:text-gray-400 dark:hover:text-fuchsia-600">
+                                        <a href="#" class="ms-1 text-sm font-medium text-white hover:text-sky-200 md:ms-2 dark:text-gray-400 dark:hover:text-sky-600">
                                             Skills Audit 2024
                                         </a>
                                     </div>
                                 </li>
                                 <li aria-current="page">
                                     <div class="flex items-center">
-                                        <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                                        <svg class="rtl:rotate-180 w-3 h-3 text-white mx-1" aria-hidden="true"
                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                             <path stroke="currentColor" stroke-linecap="round"
                                                 stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                                         </svg>
-                                        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                                        <span class="ms-1 text-sm font-medium text-white md:ms-2 dark:text-gray-400">
                                             Organisational Report
                                         </span>
                                     </div>
@@ -55,15 +55,15 @@
                         <div class="container p-4 overflow-auto"></div>
                     </div>
                     <div class="flex justify-between mb-1">
-                        <span class="text-base font-medium text-fuchsia-700 dark:text-white">
+                        <span class="text-base font-medium text-sky-700 dark:text-white">
                             {{$assessmentProgress["completed"]}}/{{$assessmentProgress["total"]}}
                         </span>
-                        <span class="text-sm font-medium text-fuchsia-700 dark:text-white">
+                        <span class="text-sm font-medium text-sky-700 dark:text-white">
                             {{$assessmentProgress["percentage"]}}%
                         </span>
                     </div>
                     <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                        <div class="bg-fuchsia-600 h-2.5 rounded-full" style="width: {{$assessmentProgress["percentage"]}}%"></div>
+                        <div class="bg-sky-600 h-2.5 rounded-full" style="width: {{$assessmentProgress["percentage"]}}%"></div>
                     </div>
                 </div>
             </div>
