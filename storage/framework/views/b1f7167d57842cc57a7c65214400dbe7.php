3bde477c9153a46cdeb210f7960443a7da018762
hpmouton <hmouton786@gmail.com>
2024-10-25 09:37:29 +0200

    Remove The Log Drum

--- storage/framework/views/b1f7167d57842cc57a7c65214400dbe7.php
+++
@@ -1,2 +0,0 @@
-<canvas style="display: none;" id="<?php echo e($chart->id); ?>" <?php echo $chart->formatContainerOptions('html'); ?>></canvas>
-<?php echo $__env->make('charts::loader', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/hubert/skillharbor-open/resources/views/vendor/charts/chartjs/container.blade.php ENDPATH**/ ?>
